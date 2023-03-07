85019ea2c2ffcf2ffc3fa28c165a44d97d75e430
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2023-03-07 14:31:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 //if the qr have any error, it mean this {whatsapp-bot-pack} package is not up-to-date 
 module.exports = {
-       MONGO_URL : '',//enter your mongodb url to get and store youe session!
-       SESSION_PREFIX : 'inrl~'// defult value of the satrting of your session
-       GROUP_URL :"https://chat.whatsapp.com/group?inrl=groupid",//group url for your aupport grp link
-       GITHUB_URL : "https://github.com/ekash.efx/inrl/fork"//github id for fork after scaning
+       MONGO_URL : 'mongodb+srv://Abu:wLnxJKwYIiF2CC0K@abu.mbnlgm8.mongodb.net/?retryWrites=true&w=majority',//enter your mongodb url to get and store youe session!
+       SESSION_PREFIX : 'Jsl~'// defult value of the satrting of your session
+       GROUP_URL :"https://chat.whatsapp.com/KDt0eEkGCho7tLbfcmMltB",//group url for your aupport grp link
+       GITHUB_URL : "https://github.com/Afx-Abu/Abu-MD"//github id for fork after scaning
 }
 /*
       EXAMPLE
